34e9d9e74fab9d6a98dfd4b2c30c9222e0be03e4
Aniket Kalvit <aniket.kalvit@9ledgepro.com>
2022-03-22 12:37:33 +0530

    commit

---
+++ ProjectStructure/MDIContainer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectStructure
+{
+    public partial class MDIContainer : Form
+    {
+        public MDIContainer()
+        {
+            InitializeComponent();
+        }
+
+        private void form1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.MdiParent = this;
+            form1.Show();
+        }
+
+        private void form2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.MdiParent = this;
+            form2.Show();
+        }
+    }
+}
--- ProjectStructure/Program.cs
+++ ProjectStructure/Program.cs
@@ -16,7 +16,7 @@ namespace ProjectStructure
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MDIContainer());
         }
     }
 }
